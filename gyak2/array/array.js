5318076237dbf42f971c3c535b078c064f15b526
barnabas.balazs <barnabas.balazs@enliven.systems>
2024-09-16 10:58:04 +0200

    2. gyakorlat WIP

--- gyak2/array/array.js
+++ gyak2/array/array.js
@@ -32,34 +32,53 @@ const students = [
 ];
 
 // 22 év feletti személyek
-const atLeast22YearsOld = undefined;
+const atLeast22YearsOld = students.filter((x) => x.age > 22);
 
 // Az egyetlen 20 év alatti személy
-const under20 = undefined;
+const under20 = students.find((x) => x.age < 20);
 
 // Mindenki 40 év alatti?
-const everyoneUnder40 = undefined;
+const everyoneUnder40 = students.every(x => x.age < 40);
 
 // Legöregebb személy
-const oldestPerson = undefined;
+const oldestPerson = students.reduce((partialMax, s) => {
+    return s.age > partialMax.age ? s : partialMax
+}, students[0]);
 
 // Az első bicikliző indexe a tömbben
-const firstCyclerIndex = undefined;
+const firstCyclerIndex = students
+    .findIndex((s) => s.hobbies.includes('Biciklizés'));
 
 // ABC-rendben a 3. személy
-const thirdPerson = undefined;
+const thirdPerson = students
+    .sort((s1, s2) =>
+        s1.name.localeCompare(s2.name))[2];
 
 // Szeretnek LEGOzni
-const likesLEGO = undefined;
+const likesLEGO = students
+    .filter((s) => s.hobbies.find(hobby => hobby === 'LEGO'));
 
 // Hobbik átlagos száma
-const averageHobbyCount = undefined;
+const averageHobbyCount = students
+    .reduce((partialSum, s) => partialSum + s.hobbies.length, 0) /
+        students.length;
 
 // Hobbik egyedi listája
-const uniqueHobbies = undefined;
+const uniqueHobbies = [
+    ...new Set(students
+        .flatMap((s) => s.hobbies)
+    )
+];
+// ['LEGO', 'Hangya', ...]
+// [ ['LEGO'], ['..'], ...].flatten
 
 // Egyedi keresztnevek
-const uniqueNames = undefined;
+const uniqueNames = students
+    .flatMap(s => {
+        const nameSplit = s.name.split(' ');
+        nameSplit.shift();
+        return nameSplit;
+    });
 
 console.log(
     `Legalább 22 évesek:`,  atLeast22YearsOld,

eba0ca1724b6b72d4bd4fd498fdf236f80c5ef2d
barnabas.balazs <barnabas.balazs@enliven.systems>
2024-09-30 10:07:45 +0200

    JavaScript ZH

---
+++ zh-js/zh1_1.js
@@ -0,0 +1,64 @@
+let students = [
+    {
+        name: "Szabó Géza Szilveszter",
+        grades: [5, 5, 5, 5],
+        start: 2021
+    },
+    {
+        name: "Zsilka Valér",
+        grades: [3, 5, 2, 4],
+        start: 2023
+    },
+    {
+        name: "Rajhona Leóna Anasztázia",
+        grades: [4, 5, 5, 4],
+        start: 2024
+    },
+    {
+        name: "Nagy Balázs",
+        grades: [5, 1, 2, 5],
+        start: 2020
+    },
+    {
+        name: "Nagy Levente",
+        grades: [1, 2, 5, 5],
+        start: 2024
+    },
+    {
+        name: "Tóth Gellért Levente",
+        grades: [4, 3, 4, 3],
+        start: 2022
+    },
+    {
+        name: "Somogyi Szilárd",
+        grades: [5, 4, 3, 3],
+        start: 2019
+    }
+]
+
+// 1.1
+const studentFrom2022 = undefined;
+
+// 1.2
+const everyStudentHasA5 = undefined;
+
+// 1.3
+const gradeAverages = undefined;
+
+// 1.4
+const secondStudent = undefined;
+
+// 1.5
+const studentsAfter2021 = undefined;
+
+// +1
+const secondBestStudent = undefined;
+
+console.log(
+    '\nA 2022-ben kezdő diák neve:', studentFrom2022,
+    '\nMinden diáknak van legalább egy ötöse:', everyStudentHasA5,
+    '\nÁtlagok:', gradeAverages,
+    '\n Második diák:', secondStudent,
+    '\n 2021-ben vagy azután kezdő diákok:', studentsAfter2021,
+    '\nA 2. legnagyobb átlaggal rendelkező diák:', secondBestStudent
+);
---
+++ zh-js/zh1_2.js
@@ -0,0 +1,5 @@
+const bookContainerEl = document.querySelector('#container');
+const errorEl = document.querySelector('#error');
+const searchInputEl = document.querySelector('#searchInput');
+
+

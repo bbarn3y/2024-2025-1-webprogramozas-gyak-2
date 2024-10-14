47221fe9e659d9a3c5d79fcc42b38863dd3211fe
barnabas.balazs <barnabas.balazs@enliven.systems>
2024-10-14 11:28:48 +0200

    Gyak 6

--- gyak6/flappy.js
+++ gyak6/flappy.js
@@ -26,7 +26,17 @@ class Bird {
         if (this.y >= canvas.height - this.height) {
             gameOver = true;
         }
+        if (columns.some((c) => rectanglesCollide(this, c))) {
+            gameOver = true;
+        }
     }
+}
+
+function rectanglesCollide(rect1, rect2) {
+    return rect1.x <= rect2.x + rect2.width &&
+        rect1.x + rect1.width >= rect2.x &&
+        rect1.y <= rect2.y + rect2.height &&
+        rect1.y + rect1.height >= rect2.y;
 }
 
 class Column {

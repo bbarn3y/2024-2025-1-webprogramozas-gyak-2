12329061439933269b060e907c881439d4dc2d6b
barnabas.balazs <barnabas.balazs@enliven.systems>
2024-11-04 10:21:48 +0100

    Gyak 8 WIP

--- gyak8/index.php
+++ gyak8/index.php
@@ -1,4 +1,36 @@
 <?php
 
+$formState = [
+        1 => [
+                'name' => 'Select one',
+                'type' => 'radio',
+                'options' => [
+                        'First option' => false,
+                        'Second option' => true
+                ]
+        ],
+        2 => [
+                'name' => 'Select multiple',
+                'type' => 'checkbox',
+                'options' => [
+                        'First' => true,
+                        'Second' => false,
+                        'Third' => true
+                ]
+        ]
+];
+
+
+?>
+
+<!--
+<h3> Radio Selector </h3>
+<input type="radio" name="test" value="1" checked> 1
+-->
+
+<?php foreach ($formState as $id => $settings): ?>
+
+<?php endforeach; ?>
+
 
 

e1e5f1f571121dd8c2ba41161570a319cd66d752
barnabas.balazs <barnabas.balazs@enliven.systems>
2024-12-02 10:18:51 +0100

    Gyak 12

--- gyak12/authentication/authenticator.php
+++ gyak12/authentication/authenticator.php
@@ -4,11 +4,29 @@ class UserStorage {
 
     // @note The password is password
     function findUser($username, $password): array {
-        // @todo Implement!
+        $users = $this->getUsers();
+        return array_filter($users, fn($user) =>
+            $user['username'] === $username &&
+            $user['password'] === md5($password)
+        );
     }
 
     function getUsers(): array {
         return json_decode(file_get_contents('users.json'), true);
     }
 }
+
+$userStorage = new UserStorage();
+
+$user = $userStorage->findUser($_POST['username'], $_POST['password']);
+
+if (count($user) === 1) {
+    session_start();
+    $_SESSION['user'] = array_shift($user);
+    header('Location: lobby.php');
+    exit();
+} else {
+    header('Location: login.php');
+}
+
 
